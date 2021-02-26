d780d9d5d074fd4cef87626d7acb368862dba367
Thomas P <43186116+T-mp@users.noreply.github.com>
2021-02-26 23:14:34 +0100

    ~IFileAdressenGenerator: Kommentare für #12

--- WebRaid.VDS/IFileAdressenGenerator.cs
+++ WebRaid.VDS/IFileAdressenGenerator.cs
@@ -1,10 +1,30 @@
 ﻿namespace WebRaid.VDS
 {
+    /// <summary>
+    /// Generiert "Adressen" für die Speicher-Knoten
+    /// </summary>
     public interface IFileAdressenGenerator
     {
+        /// <summary>
+        /// Gibt eine zufällige nicht näher spezifizierte Adresse zurück
+        /// </summary>
         string GetNew();
+        /// <summary>
+        /// Gibt eine zufällige in <paramref name="blockGröße"/> unterteilte Adresse zurück.
+        /// </summary>
+        /// <param name="blockGröße">Blockgröße zum unterteilen der Adresse</param>
         string GetNew(int blockGröße);
+        /// <summary>
+        /// Gibt eine mit <paramref name="separator"/> unterteilte Adresse zurück
+        /// </summary>
+        /// <param name="separator">Seperierungs-Zeichen</param>
         string GetNew(string separator);
+
+        /// <summary>
+        /// Gibt eine zufällige in <paramref name="blockGröße"/> mit <paramref name="separator"/> unterteilte Adresse zurück.
+        /// </summary>
+        /// <param name="blockGröße">Blockgröße zum unterteilen der Adresse</param>
+        /// <param name="separator">Seperierungs-Zeichen</param>
         string GetNew(int blockGröße, string separator);
     }
 }
